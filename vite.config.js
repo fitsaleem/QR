a81352881619ff2b9ae9edabb51436994fca30f2
elnurnabivi <elnurnabivi@gmail.com>
2023-12-27 22:39:43 +0100

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/QR/",
+  base: "/",
   plugins: [
     react(),
     commonjs({
