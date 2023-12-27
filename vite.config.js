cca8fcb35f860f034a413f9611b00a1cb737331f
elnurnabivi <elnurnabivi@gmail.com>
2023-12-27 21:14:07 +0100

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/QR/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs({
+      filter(id) {
+        if (id.includes("node_modules/qrcode-with-logos")) {
+          return true;
+        }
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
 });
